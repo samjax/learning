4b00554ea7ddd924acd753775e491f0f39aa865d
Sampath <hisampath@gmail.com>
2019-04-30 19:11:38 +0530

    added hadoop admin infomation

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public class hello
   public static void main(String[] args)
   {
     int age;
-    System.out.println("Hello my age is" + age);
+    System.out.println("Hello my age is :" + age);
+    System.out.println(" I am hadoop admin ");
   }
 }
