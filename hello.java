c20fb24ae52260c8c4a89f2fe6ca3191affc66e2
Sampath <hisampath@gmail.com>
2019-04-30 19:16:41 +0530

    Added a line for new branch

--- hello.java
+++ hello.java
@@ -5,5 +5,6 @@ public class hello
     int age;
     System.out.println("Hello my age is :" + age);
     System.out.println(" I am hadoop admin ");
+    System.out.println("Adding this for a new branch")
   }
 }
